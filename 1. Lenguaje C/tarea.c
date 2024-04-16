751fea7de605ef60d8f5f585170a401b277bce31
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-04-16 16:38:47 +0000

    a homework

---
+++ 1. Lenguaje C/tarea.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Ejercicios tipo de dato char
+// 1-	Imprime los caracteres 'A', 'B', 'C' y sus códigos ASCII correspondientes.
+// 2-	Pide al usuario que ingrese dos caracteres y luego imprime la suma de sus códigos ASCII.
+// 3-	Pide al usuario que ingrese un carácter en mayúscula y luego imprime su equivalente en minúscula.
+
+void main()
+{
+
+}
