2d3e18e1e6a999599b5bd207cdae66516c3b76a6
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-05-08 22:35:25 +0000

    ejercicio 4 practica en C

--- 1. Lenguaje C/Unidad4_Array/practica en C/ejercicio4_Array.c
+++ 1. Lenguaje C/Unidad4_Array/practica en C/ejercicio4_Array.c
@@ -6,13 +6,37 @@ int main()
     int matriz[6] = {10, 20, 5, 15, 30, 20};
     int mayor_numero = matriz[0];
     int suma = 0;
-    int contador = 0;
+    int acumulador = 0;
     int i;
 
     for(i = 0; i < 6; i++) {
-        printf("ÍNDICE: %d -> VALOR: %d\n", i, matriz[i]);
+        // Informar el vector de la forma: "Índice: X, Valor: Y".
+        printf("a. ÍNDICE: %d -> VALOR: %d\n\n", i, matriz[i]);
+
+        // Totalizar el vector e informar el total.
+        suma += matriz[i];
+
+        // Informar el contenido de las posiciones impares.
+        if (matriz[i] % 2 == 1) {
+            printf("c. d. Los numeros impares y sus posiciones son: índice: %d - valor: %d \n", i, matriz[i]);
+        };
+
+        // Informar el mayor número
+        if (i == 0) {
+            mayor_numero = matriz[i];
+        } else if (mayor_numero > matriz[i]) {
+            mayor_numero = matriz[i];
+        }
+        
+        if(matriz[i] == 20) {
+            acumulador = acumulador +1;
+        }
+        
     }
 
+    printf("b. La suma es: %d \n\n", suma);
+    printf("e. El mayor número es: %d\n", mayor_numero);
+    printf("f. El número 20 aparece: %d veces \n", acumulador);
 
     return 0;
 }
