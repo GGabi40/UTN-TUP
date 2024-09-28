40380c0dd533d550de8e332b6763313b9a0d6815
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-09-28 00:19:01 +0000

    ejercicios python 2

--- 2-Cuatri/Unid_5_Python/2_ejercicios.py
+++ 2-Cuatri/Unid_5_Python/2_ejercicios.py
@@ -128,14 +128,26 @@ números de manera continua hasta que ingrese un número
 negativo. El programa debe multiplicar todos los números 
 ingresados y mostrar el resultado al final."""
 
+acumulador = 1
+while True:
+    numeros = int(input('Ingrese un numero positivo: \nIngrese -1 para salir\n'))
+    if(numeros < 0):
+        break
+    else:
+        acumulador *= numeros
 
+print(f'El acumulador de todos los numeros ingresados es de: {acumulador}')
 
 
 """ Ejercicio 7: Cuenta regresiva
 Escribe un programa que pida un número al usuario y 
 realice una cuenta regresiva desde ese número hasta 0, 
 imprimiendo cada número en pantalla."""
- 
+
+numero = int(input('Ingrese un numero para la cuenta regresiva: '))
+
+for i in reversed(range(numero)):
+    print(i)
 
  
 """ Ejercicio 8: Lista de palabras
@@ -143,6 +155,15 @@ Escribe un programa que pida al usuario ingresar 5
 palabras y las almacene en una lista. Luego, imprime 
 las palabras en orden inverso al que fueron ingresadas."""
 
+palabras = []
+
+print('Ingrese 5 palabras: ')
+for i in range(5):
+    nuevaPalabra = input(f'Palabra {i+1}: ').capitalize()
+    palabras.append(nuevaPalabra)
+
+# revertido = list(reversed(palabras))
+print(f'La lista de palabras en el orden inverso: {list(reversed(palabras))}')
 
 
 """Ejercicio 9: Números primos
@@ -151,7 +172,27 @@ números primos entre 1 y 50 utilizando un bucle for.
 Un número primo es aquel que solo es divisible por 1 
 y por sí mismo."""
 
+numero = list(range(1,50))
 
+# numero == 1
+# i == 1 | numero % 1 == 0 ? True
+# i == 2 | numero % 2 == 0 ? 
+# i == 3 | numero % 3 == 0 ? 
+# i == 4 | numero % 4 == 0 ? 
+# i == 5 | numero % 5 == 0 ? 
+# i HASTA numero | numero % i ? Si es True, no es primo.
+
+for numero in range(2, 51):
+    esPrimo = True
+    
+    for i in range(2, numero):
+        if(numero % i == 0):
+            esPrimo = False
+            break
+    
+    if(esPrimo):
+        print(f'{numero} es primo')
+        
 
 """Ejercicio 10: Calificación de notas
 Crea un programa que pida al usuario ingresar 
@@ -160,4 +201,36 @@ Luego, el programa debe imprimir cuántas calificaciones
 son mayores o iguales a 6 (aprobadas) y cuántas son menores 
 (reprobadas). """
 
+print('<>' *20)
+print('\n\033[4;33mCALIFICACIÓN DE NOTAS\033[m\n')
+print('<>' *20)
 
+notas = []
+
+for i in range(1,6):
+    while True:
+        nota = int(input(f'Ingrese la nota \033[32m{i}\033[m: '))
+        
+        if(nota < 0 or nota > 10):
+            print('La nota debe ser mayor a 10 o menor que 0.')
+        else:
+            notas.append(nota)
+            break
+
+print(f'Lista de notas: {notas}')
+
+contadorBajo = 0
+contadorAlto = 0
+for elemento in notas:
+    if(elemento >= 6):
+        contadorAlto += 1
+    else:
+        contadorBajo += 1
+        
+print(f'\033[1;32mNOTAS APROBADAS: \033[m{contadorAlto} notas.')
+print(f'\033[1;33mNOTAS REPROBADAS: \033[m{contadorBajo} notas.')
+
+
+# EL CÓDIGO \033[m 
+# AGREGA COLOR AL PRINT DE LA TERMINAL.
+# \033[1;33m -> significa 1: bold // 33: texto amarillo
