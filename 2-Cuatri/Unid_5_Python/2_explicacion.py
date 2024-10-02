665a79234493a94dc0c49909e097807c06de211c
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-10-02 19:22:38 +0000

    Explicacion array y Diccionarios

--- 2-Cuatri/Unid_5_Python/2_explicacion.py
+++ 2-Cuatri/Unid_5_Python/2_explicacion.py
@@ -19,3 +19,46 @@ print(array.index(2)) # Muestra la posición del elemento entre paréntesis
 # ----------------------
 # Diccionarios | Objetos
 
+diccionario = {'Juan': 56, 'Ana': 15}
+print(diccionario)
+
+diccionarioFuncionAnonima = {x: x**2 for x in (2, 4, 6)}
+print(diccionarioFuncionAnonima)
+
+diccionarioVacio = {}
+
+
+# Clave:valor || Key:Value
+
+juegosOlimpicos = {
+    "handball": 20,
+    "natacion": 60, # Clave: Valor
+    "triatlon": 10
+}
+
+print(juegosOlimpicos.items()) # Me imprime toda la clave, valor
+print(juegosOlimpicos.keys()) # Me imprime solo las claves
+print(juegosOlimpicos.values()) # Me imprime solo los valores
+
+for claves, valor in juegosOlimpicos.items():
+    print(claves, ': ', valor)
+
+
+# Arreglo de Diccionario
+
+personas = [{'nombre': 'Jorge', 'edad': 42},
+            {'nombre': 'Ana', 'edad': 22},
+            {'nombre': 'Britany', 'edad': 32}]
+
+print(personas[1]["edad"])
+print(personas[2].get("nombre"))
+
+print('-'*10)
+print(personas[2].get("direccion")) # Devuelve NONE
+# print(personas[2]["direccion"]) # Devuelve un ERROR
+
+print('-'*10)
+
+for i, persona in enumerate(personas,start=1):
+    print(f'El valor de i: {i}')
+    print(persona)
