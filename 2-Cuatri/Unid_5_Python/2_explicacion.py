91943be667261551e141335aafa8b74ab7e66a3b
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-10-02 18:55:41 +0000

    update archivos

---
+++ 2-Cuatri/Unid_5_Python/2_explicacion.py
@@ -0,0 +1,21 @@
+# Arreglos
+
+array = [1,2,3]
+print(array)
+
+array.append('Hola') # Me agrega el elemento 'Hola' al array
+print(array)
+
+array.remove(3) # Elimina el elemento entre paréntesis de la lista.
+print(array)
+
+array.pop() # Elimina el último elemento de la lista.
+print(array)
+
+
+print(array.index(2)) # Muestra la posición del elemento entre paréntesis
+
+
+# ----------------------
+# Diccionarios | Objetos
+
