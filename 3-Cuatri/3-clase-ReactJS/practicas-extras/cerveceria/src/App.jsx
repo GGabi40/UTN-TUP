a5e2c25637561d564e7478fe27328f1605c8fd32
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2025-03-27 18:22:36 -0300

    clase 27-03-2025

--- 3-Cuatri/3-clase-ReactJS/practicas-extras/cerveceria/src/App.jsx
+++ 3-Cuatri/3-clase-ReactJS/practicas-extras/cerveceria/src/App.jsx
@@ -1,79 +1,39 @@
 import "./App.css";
-import beers from './data/beers.js';
+import beers from "./data/beers.js";
 
-import { Beer } from "./components/beer/Beer"
-import ChangeDollar from './components/changeDollar/ChangeDollar'
+import { useState } from "react";
+
+import { Beer } from "./components/beer/Beer";
+import ChangeDollar from "./components/changeDollar/ChangeDollar";
 import BeerStyle from "./components/BeerStyle";
 import QuantityStyle from "./components/QuantityStyle";
 
-
 /* Practicas adicionels */
-import AllProducts from "./components/additional-practice-1/AllProducts";
-import Persons from "./components/additional-practice-2/Persons";
-import Family from "./components/additional-practice-3/Family";
-import { useState } from "react";
+import AdditionalPractices from "./components/additional-practices/AdditionalPractices";
 
 function App() {
   const [exchangeRate, setExchangeRate] = useState(1200);
 
-  /*  Practicas adicionales  */
-  const products = ["table","couch","chair1","chair2"];
-
-  const names = ["Parker","Simmons","Lewis","Poe"];
-
-  const persons = [
-    { name: 'Juan', age: 26 },
-    { name: 'Gabriel', age: 27 },
-    { name: 'Valentina', age: 22 },
-    { name: 'Paula', age: 25 },
-    { name: 'Andrés', age: 20 }
-  ];
-
-  const personsOrganized = persons.sort((a,b) => b.age - a.age);
-  /* Fin practicas adicionales  */
-
   return (
     <>
-    <div className="practica-cerveceria">
-      <h1>Cervecería 🍻</h1>
-      <ChangeDollar exchangeRate={exchangeRate} setExchangeRate={setExchangeRate} />
+      <div className="practica-cerveceria">
+        <h1>Cervecería 🍻</h1>
+        <ChangeDollar
+          exchangeRate={exchangeRate}
+          setExchangeRate={setExchangeRate}
+        />
 
-      <h2>Nuestro menú:</h2>
-      <Beer beers={beers} exchangeRate={exchangeRate} />
+        <h2>Nuestro menú:</h2>
+        <Beer beers={beers} exchangeRate={exchangeRate} />
 
-      <div className="quantity">
         <QuantityStyle beers={beers} />
-      </div>
 
-      <div className="styles">
         <BeerStyle beers={beers} />
       </div>
-    </div>
 
-    <hr />
-
-    {/* --- Practicas adicionales--- */}
-    <div className="practica-adicional">
-      <h1>Práctica Adicional 1</h1>
-      <AllProducts products={products} />
-    </div>
-
-    
-    <hr />
-
-    <div className="practica-adicional">
-      <h1>Práctica Adicional 2</h1>
-      <Persons personsName={names} />
-    </div>
-
-    <hr />
-
-    <div className="practica-adicional">
-      <h1>Práctica Adicional 3</h1>
-      <Family persons={personsOrganized} />
-    </div>
-
-    {/* FIN Practicas adicionales */}
+      {/* Practicas adicionales: */}
+      <hr />
+      <AdditionalPractices />
     </>
   );
 }
---
+++ 3-Cuatri/3-clase-ReactJS/practicas-extras/cerveceria/src/components/additional-practices/AdditionalPractices.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import AllProducts from "../../components/additional-practice-1/AllProducts";
+import Persons from "../../components/additional-practice-2/Persons";
+import Family from "../../components/additional-practice-3/Family";
+
+function AdditionalPractices() {
+  const products = ["table","couch","chair1","chair2"];
+
+  const names = ["Parker","Simmons","Lewis","Poe"];
+
+  const persons = [
+    { name: 'Juan', age: 26 },
+    { name: 'Gabriel', age: 27 },
+    { name: 'Valentina', age: 22 },
+    { name: 'Paula', age: 25 },
+    { name: 'Andrés', age: 20 }
+  ];
+
+  const personsOrganized = persons.sort((a,b) => b.age - a.age);
+
+  return (
+    <div>
+      {/* --- Practicas adicionales--- */}
+      <div className="practica-adicional">
+        <h1>Práctica Adicional 1</h1>
+        <AllProducts products={products} />
+      </div>
+
+      <hr />
+
+      <div className="practica-adicional">
+        <h1>Práctica Adicional 2</h1>
+        <Persons personsName={names} />
+      </div>
+
+      <hr />
+
+      <div className="practica-adicional">
+        <h1>Práctica Adicional 3</h1>
+        <Family persons={personsOrganized} />
+      </div>
+
+      {/* FIN Practicas adicionales */}
+    </div>
+  );
+}
+
+export default AdditionalPractices;
